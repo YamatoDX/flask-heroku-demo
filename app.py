8a5338e6602e74a9bb576a31f6032379c516f50a
Md Shadman Matin (Piyal) <78369801+YamatoDX@users.noreply.github.com>
2022-08-26 09:27:51 +0600

    app file updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__);
 
 @app.route("/")
 def index():
-    return "go piyal go!"
+    return "Sample Data from this project"
 
 if __name__ == "__main__":
     app.run(debug = True)
